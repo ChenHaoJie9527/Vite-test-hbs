2c1e1d8aaf3b9cb8358a7719fb72440f5f9b7664
chenhaojie <a18520145493@163.com>
2022-04-15 18:42:28 +0800

    报错

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from "path";
+// 引入 path 包注意两点:
+// 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
+// 2. tsconfig.node.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'), // // 手动指定项目根目录位置
   plugins: [vue()]
 })
