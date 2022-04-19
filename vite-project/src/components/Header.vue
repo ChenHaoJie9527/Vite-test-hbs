f383ab6d1674cc5ea56d359138047440465d59c8
chenhaojie <a18520145493@163.com>
2022-04-19 16:40:11 +0800

    配置eslint prettier

--- vite-project/src/components/Header.vue
+++ vite-project/src/components/Header.vue
@@ -1,10 +1,9 @@
 <template>
-    <header class="header bg-red-400">header123</header>
+  <header class="header bg-red-400">header123</header>
 </template>
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 <style lang="scss" scoped>
 .header {
-    color: $theme-color;
+  color: $theme-color;
 }
 </style>
--- vite-project/src/components/NavBar.tsx
+++ vite-project/src/components/NavBar.tsx
@@ -1,14 +1,14 @@
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue';
 // import style from "../styles/navbar.module.scss";
-import style from "@/styles/navbar.module.scss";
+import style from '@/styles/navbar.module.scss';
 
 export default defineComponent({
-    name: 'NavBar',
-    render() {
-        return (
-          <div class={style.NavBart}>
-            NavBart <span class={style["NavBart.active"]}>跟我走吧</span>
-          </div>
-        );
-    }
-})
+  name: 'NavBar',
+  render() {
+    return (
+      <div class={style.NavBart}>
+        NavBart <span class={style['NavBart.active']}>跟我走吧</span>
+      </div>
+    );
+  },
+});
